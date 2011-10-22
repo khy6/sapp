ef3d8c40c4b8f55558a6aa0052e61ffeb28ebcde
khy6 <khy.trading@gmail.com>
2011-10-22 14:04:42 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sapp::Application.config.secret_token = '6d7019a4e26bc428adcd3cc280d4033a8e0d0b060d843c435a6bf0bac8106f5f6d714cf4f989aa9e63ea754c77703df41fb99d2df45951bc0980d2dd5f32dc70'
